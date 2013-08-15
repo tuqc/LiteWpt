d831af3e9fbba721ecfd5dc5d3677251a232684a
Qichen Tu <tuqc@google.com>
2013-08-15 10:28:15 +0800

    Add node and chrome pkg.

--- agent.sh
+++ agent.sh
@@ -6,6 +6,7 @@ export WPT_MAX_LOGLEVEL="debug"
 
 # Use the latest WebDriver javascript
 #declare -a wdjs_dirs=("${wpt_root}/lib/webdriver/javascript/node-"*)
+export NODE="$ROOT_DIR/pkgs/node-v0.10.15-linux-x64/bin/node"
 export NODE_PATH="${NODE_PATH}:${ROOT_DIR}/src"
 
 declare -a selenium_jars=("${ROOT_DIR}/lib/selenium-standalone-"*.jar)
@@ -18,4 +19,4 @@ export cmd_arg="--browser ${browser} --chromedriver ${chromedriver} --seleniumJa
 
 echo "Run with ${cmd_arg}"
 
-node ${ROOT_DIR}/src/agent_main.js ${cmd_arg}
+$NODE ${ROOT_DIR}/src/agent_main.js ${cmd_arg}
